4f7528a67d9362a1ad6f6a66cdd3cae669f88808
raboliotthegreat <raboliotthegreat@mailoo.org>
2017-11-06 10:31:49 +0100

    server UDP done

--- Server/module/serverUDP.py
+++ Server/module/serverUDP.py
@@ -1,15 +1,23 @@
-from socketserver import UDPServer, ThreadingMixIn, DatagramRequestHandler
+import socket
+import threading
 
-class serverUDP():
-    def __init__(self):
-        self.protocole = None
-        self.port = None
+class ServerUDP():
+    def __init__(self, port):
+        self.run = True
+        self.port = port
 
+        self.address = ('0.0.0.0', self.port)
+        self.cnx = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        #Then we run
         self.__run()
 
     def __run(self):
-        pass
-    def __newClient(self):
-        pass
+        cnx.bind(self.address)
+        while self.run:
+            (req, addr) = cnx.recvfrom(1024)
+            # We could stock each thread + infos, but it won't be useful
+            threading.Thread(target=self.__newClient, args = [req, addr]).start()
+    def __newClient(self, req, addr):
+        self.__send(addr) # Not really useful but it look nicer
     def __send(self):
-        pass
+        self.cnx.sendto( bytearray("OK" + "\t", "ascii"), addr)
